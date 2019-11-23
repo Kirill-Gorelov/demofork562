9b959ee897b4def8739da1d2f1e69fdb210b9c31
gorelov <gorelov@ener.ru>
2019-11-23 20:15:44 +0300

    работа  категориями

--- src/Backend/Modules/EnerIblocks/Actions/Edit copy 2.php
+++ src/Backend/Modules/EnerIblocks/Actions/Edit copy 2.php
@@ -21,23 +21,29 @@ class Edit extends BackendBaseActionEdit {
         $this->product = $this->get('doctrine')->getRepository(Category::class)->getCategory($this->id);
     }
 
-    private function prepareForm(){
-        var_dump($this->product);
+    // private function parseForm(Form $form): void
+    // {
+    //     $this->template->assign('form', $form->createView());
 
+    //     $this->parse();
+    //     $this->display();
+    // }
+
+    private function preareForm(){
+        var_dump($this->product);
+        die;
     } 
 
     private function loadForm(){
         $this->form = new BackendForm('edit');
-        // $this->form->addText('title', $this->product['title'], 512, 'form-control title', 'form-control danger title');
-        // $this->form->addText('title2', $this->product['title'], 512, 'form-control title', 'form-control danger title');
+        $this->form->addText('title', $this->product->getTitle(), 512, 'form-control title', 'form-control danger title');
+        $this->form->addText('title2', $this->product->getTitle(), 512, 'form-control title', 'form-control danger title');
         
         // $variable = ['title'];
         // $type_variable = ['title'=>'addText'];
         // foreach ($variable as $key => $value) {
         //     $this->form->$type_variable[$value]($value, $this->product->getTitle(), 512, 'form-control title', 'form-control danger title');
         // }
-
-        $this->prepareForm();
 
     }
 
@@ -51,23 +57,41 @@ class Edit extends BackendBaseActionEdit {
         $this->template->assign('deleteForm', $deleteForm->createView());
     }
 
+    // protected function parse(): void
+    // {
+    //     parent::parse();
+    //     // $this->template->assign('mediaGroup', $this->product->getMediaGroup());
+    //     // $this->template->assign('image', $this->product->getImage());
+    //     $this->template->assign('id', $this->product->getId());
+    // }
+
     public function execute(): void
     {
         parent::execute();
+        // $this->header->addJS($_SERVER['DOCUMENT_ROOT'].'/src/Backend/Modules/EnerIblocks/js', 'Core', false);
+        // $this->header->addJS($_SERVER['DOCUMENT_ROOT'].'/src/Backend/Modules/EnerIblocks/js', 'EnerIblocks', false);
+        // $this->header->addJS('jsonform.js', 'EnerIblocks', false);
+        // $this->header->addJS('jsonform-split.js', 'EnerIblocks', false);
+        // $this->header->addJS('jsonform-defaults.js', 'EnerIblocks', false);
+        
+        // $this->header->addCSS('http://cdn.jsdelivr.net/npm/alpaca@1.5.27/dist/alpaca/bootstrap/alpaca.min.css', '', false);
+        
+        // $this->header->addJS('http://code.jquery.com/jquery-1.11.1.min.js', '', false);
+        // $this->header->addJS('http://cdn.jsdelivr.net/npm/alpaca@1.5.27/dist/alpaca/bootstrap/alpaca.min.js', '', false);
         $this->header->addJS('myjs.js', 'EnerIblocks', false);
         
+        // FRONTEND_FILES_PATH
         $this->id = $this->getRequest()->get('id');
 
         $this->loadProduct();
-        $this->loadForm();
-        
+        // $this->loadForm();
 
         $obj_label = [];
         $obj_label['title'] = 'заголовок';
         // $obj_label['description'] = 'Описание';
 
         // dump((object) $obj_label);
-        $this->template->assign('idw', $this->product['id']);
+        // $this->template->assign('idw', $this->product->getId());
         $this->template->assign('obj_label', $obj_label);
 
         if ($this->form->isSubmitted()) {
@@ -75,9 +99,8 @@ class Edit extends BackendBaseActionEdit {
             // $this->parseForm($form);
 
             parent::parse();
-            $this->template->assign('id', $this->product['id']);
+            $this->template->assign('id', $this->product->getId());
             $this->display();
-            var_dump($this->getRequest()->get('username'));
     
             // $this->get('doctrine')->getRepository(Category::class)->update();
             $item = [
@@ -86,7 +109,7 @@ class Edit extends BackendBaseActionEdit {
 
             // dump($item);
             // die;
-            // $this->get('doctrine')->getRepository(Category::class)->customsave($this->id, $item);
+            $this->get('doctrine')->getRepository(Category::class)->customsave($this->id, $item);
             // $this->redirect(BackendModel::createUrlForAction('Category'));
             // return;
         }
---
+++ src/Backend/Modules/EnerIblocks/Js/myjs.js
@@ -0,0 +1,16 @@
+$(window).load(function() { 
+// {"title":"Person", "type":"object","properties":{"name":{"type":"string","description":"First and Last name","minLength":4,"default":"Jeremy Dorn"}}}
+
+var f = document.getElementById('editor');
+var i = document.createElement("input"); //input element, text
+i.setAttribute('type',"text");
+i.setAttribute('name',"username");
+
+var s = document.createElement("input"); //input element, Submit button
+s.setAttribute('type',"submit");
+s.setAttribute('value',"Submit");
+
+f.appendChild(i);
+f.appendChild(s);
+
+});
