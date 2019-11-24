2ee4c2a2d06b91a48e3a5b9c96ffc2f3b647f2e0
gorelov <gorelov@ener.ru>
2019-11-24 19:40:39 +0300

    работа  категориями

--- src/Backend/Modules/EnerIblocks/Domain/CategoryElements/CategoryElementsRepository.php
+++ src/Backend/Modules/EnerIblocks/Domain/CategoryElements/CategoryElementsRepository.php
@@ -6,57 +6,22 @@ use Doctrine\ORM\EntityRepository;
 use Backend\Core\Engine\Model as BackendModel;
 
 
-class CategoryRepository extends EntityRepository
+class CategoryElementRepository extends EntityRepository
 {
     public function update()
     {
         $this->getEntityManager()->flush();
     }
 
-    public function add(Category $Category)
+    public function add(CategoryElements $CategoryElements)
     {
-        $this->getEntityManager()->persist($Category);
+        $this->getEntityManager()->persist($CategoryElements);
         $this->getEntityManager()->flush();
     }
 
-    public function delete(Category $Category): void
+    public function delete(CategoryElements $CategoryElements): void
     {
-        $this->getEntityManager()->remove($Category);
+        $this->getEntityManager()->remove($CategoryElements);
         $this->getEntityManager()->flush();
     }
-
-    public function getAllCategoryActive(string $category){
-        return  $this->createQueryBuilder('c')
-        ->where('c.maincategory = :category')
-        ->setParameter('category', $category)
-        ->getQuery()
-        ->getResult();
-    }
-
-    public function getCategory(int $id){
-        return (array) BackendModel::getContainer()->get('database')->getRecord(
-            'SELECT * FROM category
-             WHERE id = ?',
-            [(int) $id]
-        );
-    }
-
-    //TODO: сделать выборку в зависимости от языка
-    public function getAllCategory(){
-        return BackendModel::getContainer()->get('database')->getRecords(
-            'SELECT * FROM category'
-        );
-    }
-
-    public function customsave(int $id, $data):void
-    {
-        BackendModel::getContainer()->get('database')->update(
-            'category',
-            $data,
-            'id = ?',
-            [$id]
-        );
-    }
-
-
 }
