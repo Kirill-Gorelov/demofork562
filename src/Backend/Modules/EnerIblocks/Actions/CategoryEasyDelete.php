fbadb8e8616174ea5a785d9ee38301e655a81b51
gorelov <gorelov@ener.ru>
2019-12-11 13:34:27 +0300

    удаление категории

--- src/Backend/Modules/EnerIblocks/Actions/CategoryEasyDelete.php
+++ src/Backend/Modules/EnerIblocks/Actions/CategoryEasyDelete.php
@@ -6,6 +6,8 @@ use Backend\Core\Engine\Base\ActionDelete as BackendBaseActionDelete;
 use Backend\Core\Engine\Model as BackendModel;
 use Backend\Modules\EnerIblocks\Domain\CategoryElements\CategoryElement;
 use Backend\Modules\EnerIblocks\Domain\CategorysMeta\CategoryMeta;
+use Backend\Modules\EnerIblocks\Engine\CSection;
+use Backend\Modules\EnerIblocks\Engine\CElement;
 
 class CategoryEasyDelete extends BackendBaseActionDelete
 {
@@ -19,15 +21,41 @@ class CategoryEasyDelete extends BackendBaseActionDelete
         $id = $this->getRequest()->get('id');
         $cti = $this->getRequest()->get('cti');
         $cat = $this->getRequest()->get('cat');
-        
-        // var_dump($id);
-        // die;
-        $product = $this->get('doctrine')->getRepository(CategoryElement::class)->findOneById($id);
 
-        $this->get('doctrine')->getRepository(CategoryElement::class)->delete($product);
-        $this->get('doctrine')->getRepository(CategoryMeta::class)->delete_meta($id);
-        
+        $section = new CSection;
+        // var_dump($r);
+        try {
+            $r = $section->delete($id);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            return ;
+        }
 
         $this->redirect(BackendModel::createUrlForAction('CategoryElementIndex', null, null, ['cti'=>$cti, 'cat'=>$cat]));
+
+
+        // оставлю для демо пока
+        /*
+        $el = new CElement;
+        $sort = ['id'=>'desc'];
+        // $sort = ['id'=>'asc'];
+        $filter = [];
+        // $filter = ['active'=>true];
+        // $filter = ['active'=>false];
+        // $filter = ['title'=>'Пица без категории'];
+        // $filter = ['title'=>'%тети'];
+        // $filter = ['title'=>'%от%'];
+        // $filter = ['code'=>'picca_bez_kategorii2'];
+        $filter = ['category'=>'7'];
+        // $filter = ['id'=>'28'];
+        // $filter = ['id'=>array('28','36')]; // ?
+        // $filter = ['id'=>'28, 36, 41']; // ?
+        // $filter = ['id'=>'28', 'category'=>7];
+        
+        $rez = $el->getList($sort, $filter);
+        // var_dump($rez);
+        var_dump(array_column($rez, 'id'));
+        // die;
+        */
     }
 }
