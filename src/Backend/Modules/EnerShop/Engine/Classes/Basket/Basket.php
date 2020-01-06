e69a9eb4703e1a66a7969b975499c1c7fcbc3008
gorelov <gorelov@ener.ru>
2020-01-06 17:23:14 +0300

    работа с корзиной

--- src/Backend/Modules/EnerShop/Engine/Classes/Basket/Basket.php
+++ src/Backend/Modules/EnerShop/Engine/Classes/Basket/Basket.php
@@ -33,24 +33,20 @@ class Basket{
             return false;
         }
 
-        $element = new CElement;
-        $this->element_id = $element->getById($item['id']);
-
-        if (!$this->element_id) {
-            return false;
-        }
-
         $basket_user = $this->get();
         $key = array_search($item['id'], array_column($basket_user['list'], 'id'));
-        var_dump($key);
-        var_dump(intval($key) >= 0);
-
         
-        // if(filter_var($key, FILTER_VALIDATE_INT) && intval($key) >= 0 ){ // потому что найденный элемент может быть на нулевом месте
         if(intval($key) >= 0  && !empty($basket_user['list'][intval($key)])){ // потому что найденный элемент может быть на нулевом месте
-            // if(intval($key) >= 0 &&  $key != false){ // потому что найденный элемент может быть на нулевом месте
-            $basket_user['list'][intval($key)]['quantity'] += $item['quantity'];
+            $basket_user['list'][$key]['quantity'] += $item['quantity'];
+            $basket_user['list'][$key]['item_price'] = intval($basket_user['list'][$key]['quantity']) * intval($basket_user['list'][$key]['price']);
         }else{
+            $element = new CElement;
+            $this->element_id = $element->getById($item['id']);
+
+            if (!$this->element_id) {
+                return false;
+            }
+
             //TODO: решить вопрос со скидками и со свойствами товаров
             $basket_user['list'][] = ['id' => $this->element_id['id'], 
                               'title' => $this->element_id['title'],
@@ -62,8 +58,6 @@ class Basket{
                               'discount_price' => 0,
                                 ];
         }
-
-        var_dump($basket_user['list']);
 
         FrontendModel::getSession()->set('basket', $basket_user['list']);
         return true;
