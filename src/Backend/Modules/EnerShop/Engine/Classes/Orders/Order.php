e2fc83111394b13e66a35a88455de48ea1ce905e
gorelov <gorelov@ener.ru>
2020-01-10 12:06:20 +0300

    оформление заказа

---
+++ src/Backend/Modules/EnerShop/Engine/Classes/Orders/Order.php
@@ -0,0 +1,26 @@
+<?
+namespace Backend\Modules\EnerShop\Engine\Classes\Orders;
+
+class Order {
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function setBasket($basket)
+    {
+        # code...
+    }
+
+    public function setDelivery($id)
+    {
+        # code...
+    }
+
+    public function setPay($id)
+    {
+        # code...
+    }
+}
+?>
