3f32e95283349e13456ef3f29290af8d67c9fbe8
gorelov <gorelov@ener.ru>
2020-01-10 15:48:28 +0300

    оформление заказа

--- src/Backend/Modules/EnerShop/Engine/Classes/Orders/Order.php
+++ src/Backend/Modules/EnerShop/Engine/Classes/Orders/Order.php
@@ -1,41 +1,65 @@
 <?
 namespace Backend\Modules\EnerShop\Engine\Classes\Orders;
-
+$lang = require ('ru.php');
 //префикс заказа
 class Order {
 
     public $order_id;
     public $error;
+    public $basket;
+    public $user_property;
+    public $data_pay;
+    public $data_delivery;
 
     public function setUserProperty(array $user_props)
     {   
-        $this->order_id = 22;
-        return $this->order_id;
+        if (empty($user_props)) {
+            $this->error[] = $this->getMessage('EMPTY_USER_PROPS');
+        }
+
+        $this->user_property = $user_props['list'];
     }
 
     public function setBasket($basket)
     {
-        $this->error;
+        if (empty($basket['list'])) {
+            $this->error[] = $this->getMessage('EMPTY_BASKET');
+        }
+
+        $this->basket = $basket['list'];
     }
 
-    public function setDelivery($id)
+    public function setDelivery($data)
     {
-        $this->error;
+        if (empty($data)) {
+            $this->error[] = 'Не выбран способ доставки';
+        }
+
+        $this->data_delivery = $data;
     }
 
-    public function setPay($id)
+    public function setPay($data)
     {
-        $this->error;
+        if (empty($data)) {
+            $this->error[] = 'Не выбран способ оплаты';
+        }
+
+        $this->data_pay = $data;
     }
 
     public function create()
     {
         if(!empty($this->error)){
-            // исключение
+            throw new Exception("Исправьте ошибки");
         }
 
-        
+        var_dump($this->user_property);
+        var_dump($this->basket);
+        var_dump($this->data_delivery);
+        var_dump($this->data_pay);
 
+        $this->order_id = 158;
+        return $this->getOrderId();
     }
 
     public function getErrors()
@@ -49,6 +73,15 @@ class Order {
             throw new Exception("Заказ не существует");
         }
         return $this->order_id;
+    }
+
+    private function getMessage($code){
+        if (empty($code)) {
+            return '';
+        }
+
+        return $code;
+        // return !(empty($lang[$code]) ? $lang[$code] : $code;
     }
 }
 ?>
