21dfa3368c8a60058413fece3d570fe485bd1815
gorelov <gorelov@ener.ru>
2020-02-10 11:43:09 +0300

    обновление списка селект

---
+++ src/Backend/Modules/EnerIblocks/Ajax/UpdateSelectValue.php
@@ -0,0 +1,25 @@
+<?php 
+namespace Backend\Modules\EnerIblocks\Ajax;
+
+use Backend\Core\Engine\Base\AjaxAction as BackendBaseAjaxAction;
+use Symfony\Component\HttpFoundation\Response;
+use Backend\Modules\EnerIblocks\Domain\CategorysMeta\CategoryMeta;
+use Backend\Core\Engine\Base\ActionEdit as BackendBaseActionEdit;
+
+
+class UpdateSelectValue extends BackendBaseAjaxAction{
+
+    public function execute():void
+    {
+        parent::execute();
+
+        $element = $this->getRequest()->get('element');
+        $data = $this->getRequest()->get('data');
+        var_export($data);
+        // $rr = $this->get('doctrine')->getRepository(CategoryMeta::class)->getDefaultMetaValueForSelect($element);
+        // $this->output(Response::HTTP_OK, ['response' => $rr], '');
+        
+    }
+}
+
+?>
